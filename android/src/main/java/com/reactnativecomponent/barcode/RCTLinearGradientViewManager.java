0a0b957a51152ef732860253e192de35261284f7
sin <379457721@qq.com>
2020-10-15 09:21:42 +0800

    Update RCTLinearGradientViewManager.java
    fix annotation error

--- android/src/main/java/com/reactnativecomponent/barcode/RCTLinearGradientViewManager.java
+++ android/src/main/java/com/reactnativecomponent/barcode/RCTLinearGradientViewManager.java
@@ -4,7 +4,6 @@ package com.reactnativecomponent.barcode;
 
 import android.app.Activity;
 import android.graphics.Color;
-import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.ViewGroup;
 
